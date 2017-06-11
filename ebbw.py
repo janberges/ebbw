61ebf02349b61ab0583f8e0cc668177792ee1654
Jan Berges <jan.berges@uni-bremen.de>
2017-06-11 21:55:41 +0200

    Use latin letters l, u, w instead of greek names lambda, mu and omega

--- ebbw.py
+++ ebbw.py
@@ -5,9 +5,9 @@ import numpy as np
 kB = 8.61733e-5
 epsilon = 1e-10
 
-def Tc(l, mu, wE, D, A=0.94, B=1.11, C=0.74, **ignore):
-    mu /= 1 + mu * np.log(D / wE)
-    return wE / (kB * A) * np.exp(-B * (1 + l) / (l - C * l * mu - mu))
+def Tc(l, u, w, D, A=0.94, B=1.11, C=0.74, **ignore):
+    u /= 1 + u * np.log(D / w)
+    return w / (kB * A) * np.exp(-B * (1 + l) / (l - C * l * u - u))
 
 def power_method(matrix):
    N = len(matrix)
@@ -43,28 +43,27 @@ def residue(x, N=10):
       return 2 / np.pi * sum((-1) ** n / x ** (2 * n + 1) / (2 * n + 1) ** 2
          for n in range(N)) + np.log(x)
 
-def eigenvalue(T, l, mu, wE, D, cutoff=15, rescale=True, **ignore):
+def eigenvalue(T, l, u, w, D, cutoff=15, rescale=True, **ignore):
    T *= kB
 
-   nE = wE / (2 * np.pi * T)
+   w /= 2 * np.pi * T
 
-   N = int(np.ceil(cutoff * nE - 0.5))
+   N = int(np.ceil(cutoff * w - 0.5))
 
-   lamda = l / (1 + (np.arange(2 * N) / nE) ** 2)
+   l /= 1 + (np.arange(2 * N) / w) ** 2
 
    if rescale:
-      mu /= 1 + mu * residue(D / ((2 * N + 1) * np.pi * T))
+      u /= 1 + u * residue(D / ((2 * N + 1) * np.pi * T))
 
    I = np.empty((N, N))
    J = np.empty((N, N))
 
    for n in range(N):
       for m in range(N):
-         I[n, m] = 2 * T * (lamda[abs(n - m)] - lamda[n + m + 1])
-         J[n, m] = 2 * T * (lamda[abs(n - m)] + lamda[n + m + 1] - 2 * mu)
+         I[n, m] = 2 * T * (l[abs(n - m)] - l[n + m + 1])
+         J[n, m] = 2 * T * (l[abs(n - m)] + l[n + m + 1] - 2 * u)
 
-   w = (2 * np.arange(N) + 1) * np.pi * T
-   W0 = np.copy(w)
+   W0 = w = (2 * np.arange(N) + 1) * np.pi * T
 
    while True:
       W = w + np.arctan2(D, W0).dot(I)
@@ -77,10 +76,10 @@ def eigenvalue(T, l, mu, wE, D, cutoff=15, rescale=True, **ignore):
    return power_method(np.arctan2(D, W) / W * J)
 
 def critical(variable='T', epsilon=1e-3, **more):
-   parameters = dict(T=10.0, l=1.0, mu=0.1, wE=0.02, D=1.0)
+   parameters = dict(T=10.0, l=1.0, u=0.1, w=0.02, D=1.0)
    parameters.update(more)
 
-   exponent = dict(T=2.3, l=-1.3, mu=4.0, wE=-2.3, D=-22.0)[variable]
+   exponent = dict(T=2.3, l=-1.3, u=4.0, w=-2.3, D=-22.0)[variable]
 
    while True:
       x = parameters[variable] * eigenvalue(**parameters) ** exponent
