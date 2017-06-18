579674fd7ce8e54345647f42d6b0f58f889ed9f5
Jan Berges <jan.berges@uni-bremen.de>
2017-06-18 15:08:38 +0200

    Add test script to compare Eliashberg theory and McMillan's formula

---
+++ test.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import ebbw
+import numpy as np
+import matplotlib.pyplot as plt
+
+N = 100
+
+TcEB = np.empty(N)
+TcMM = np.empty(N)
+
+variables = [
+   ('l', np.linspace(0.50, 2.50, N), r'$\lambda$'),
+   ('u', np.linspace(0.00, 0.30, N), r'$\mu$'),
+   ('w', np.linspace(0.01, 0.03, N), r'$\omega_{\mathrm{E}} / \mathrm{eV}$'),
+   ('E', np.linspace(1e-1, 1e+1, N), r'$E / \mathrm{eV}$'),
+   ]
+
+figure = plt.figure()
+
+for n, (key, x, label) in enumerate(variables, 1):
+   parameters = dict(l=1.0, u=0.1, w=0.02, E=1.0)
+
+   for i, parameters[key] in enumerate(x):
+      print '%s = %g' % (key, parameters[key])
+
+      TcEB[i] = parameters['T'] = ebbw.critical(**parameters)
+      TcMM[i] = ebbw.Tc(**parameters)
+
+   plt.subplot(2, 2, n)
+
+   plt.plot(x, TcEB, label='Eliashberg')
+   plt.plot(x, TcMM, label='McMillan')
+
+   plt.legend(loc=0, frameon=False)
+
+   plt.xlabel(label)
+   plt.ylabel(r'$T_{\mathrm{c}} / K$')
+
+plt.tight_layout()
+
+figure.savefig('test.pdf')
