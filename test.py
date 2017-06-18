3aeab56c87919503849bbf0f409767d20dcbd01f
Jan Berges <jan.berges@uni-bremen.de>
2017-06-18 19:44:10 +0200

    Improve some details in test script

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import ebbw
 import numpy as np
 import matplotlib.pyplot as plt
 
-N = 100
+N = 101
 
 TcEB = np.empty(N)
 TcMM = np.empty(N)
@@ -16,11 +16,11 @@ variables = [
    ('E', np.linspace(1e-1, 1e+1, N), r'$E / \mathrm{eV}$'),
    ]
 
-figure = plt.figure()
-
 defaults = dict(l=1.0, u=0.1, w=0.02, E=1.0)
 
-for n, (key, x, label) in enumerate(variables, 1):
+figure = plt.figure()
+
+for position, (key, x, label) in enumerate(variables, 221):
    parameters = defaults.copy()
 
    for i, parameters[key] in enumerate(x):
@@ -29,15 +29,15 @@ for n, (key, x, label) in enumerate(variables, 1):
       TcEB[i] = parameters['T'] = ebbw.critical(**parameters)
       TcMM[i] = ebbw.Tc(**parameters)
 
-   plt.subplot(2, 2, n)
+   plt.subplot(position)
 
    plt.plot(x, TcEB, label='Eliashberg')
    plt.plot(x, TcMM, label='McMillan')
 
-   plt.legend(loc=0, frameon=False)
-
    plt.xlabel(label)
-   plt.ylabel(r'$T_{\mathrm{c}} / K$')
+   plt.ylabel(r'$T_{\mathrm{c}} / \mathrm{K}$')
+
+plt.legend(loc='best', frameon=False)
 
 plt.suptitle(r'(The default parameters are $\lambda = %(l)g$, $\mu = %(u)g$, '
    r'$\omega_{\mathrm{E}} = %(w)g\,\mathrm{eV}$ and $E = %(E)g\,\mathrm{eV}$)'
