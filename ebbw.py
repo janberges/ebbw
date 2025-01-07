69c9c68cf5b77c640cbdf75ad2f2f1851296de78
Jan Berges <jan.berges@uni-bremen.de>
2025-01-07 15:20:44 +0100

    Update years of copyright

--- ebbw.py
+++ ebbw.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright (C) 2017-2024 Jan Berges
+# Copyright (C) 2017-2025 Jan Berges
 # This program is free software under the terms of the GNU GPLv3 or later.
 
 __version__ = '2021.1'
